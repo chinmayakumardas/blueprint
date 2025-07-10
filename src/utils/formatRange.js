701edbb9a4d0a8f4c8f298eabbd79e2eef9cbeb3
chinmayakumardas <chinmayakumardas2000@gmail.com>
2025-07-10 22:26:40 +0530

    deleted all

--- src/utils/formatRange.js
+++
@@ -1,8 +0,0 @@
-// lib/formatRange.js
-export function formatRange(start, end) {
-  const options = { month: 'short', day: 'numeric' };
-  const startStr = start.toLocaleDateString(undefined, options);
-  const endStr = end.toLocaleDateString(undefined, options);
-
-  return `${startStr} – ${endStr}`;
-}
--- src/utils/notification.js
+++
@@ -1,5 +0,0 @@
-// import { toast } from 'sonner';
-
-// export const notifySuccess = (msg) => toast.success(msg);
-// export const notifyError = (msg) => toast.error(msg);
-// export const notifyInfo = (msg) => toast(msg);
